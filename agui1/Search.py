1bacb00783795739f2b69204e0142e6217d7c62d
Jeremy Aguillon <evilduck51@yahoo.com>
2016-05-08 15:53:51 -0400

    updated code from python 2 to 3 and fixed some logic errors

--- agui1/Search.py
+++ agui1/Search.py
@@ -1,11 +1,13 @@
+# coding: utf-8
+
 # Jeremy Aguillon
 # CMSC 471
 # Project 1
 # Due 2/15/2016
 
 # imports queues for BFS and UCS
-from Queue import Queue
-from Queue import PriorityQueue
+from queue import Queue
+from queue import PriorityQueue
 # imports sys for command line arguments (argv)
 import sys
 
@@ -28,8 +30,9 @@ FILE_IO = -1
 MISSING_NODE = -2
 MISSING_START = -3
 MISSING_END = -4
+NO_NEIGHBORS = -5
+NO_PATH = ""
 
-    
 
 # getNodes() takes in a filename and parses the file to create nodes for each of the inputs in the file
 #            This also validates that the file exists and can be opened, and the start and end nodes are
@@ -77,7 +80,10 @@ def getNodes(Filename, start, end):
 
     # returns the dictionary if the nodes exist
     if foundStart and foundEnd:
-        return nodeDict
+        if start in nodeDict.keys():
+            return nodeDict
+        else:
+            return NO_NEIGHBORS
     # returns an error message otherwise
     elif foundStart:
         return MISSING_END
@@ -87,34 +93,6 @@ def getNodes(Filename, start, end):
         return MISSING_NODE
 
 
-
-# writePath() writes the final path it takes to search from start to end to a new file 
-# Input: outFile - the filename of the file to be written to
-#        finalPath - a list of the nodes of the path from start to end nodes
-# Output: None
-def writePath(outFile, finalPath):
-    outFile = open(outFile, 'w')
-
-    for node in finalPath:
-        outFile.write("{0}\n".format(node))
-
-    outFile.close()
-
-
-
-'''
-Based on pseudocode from Wikipedia
-https://en.wikipedia.org/wiki/Depth-first_search
-1  procedure DFS-iterative(G,v):
-2      let S be a stack
-3      S.push(v)
-4      while S is not empty
-5            v = S.pop()
-6            if v is not labeled as discovered:
-7                label v as discovered
-8                for all edges from v to w in G.adjacentEdges(v) do
-9                    S.push(w)
-'''
 # DFS() uses a graph to search depth first to find a path to a given end node 
 #       from a start node and returns the path as a list
 # Input: nodeDict - a dictionary of nodes representing a graph
@@ -123,16 +101,16 @@ https://en.wikipedia.org/wiki/Depth-first_search
 # Output: a list of the path from start to end 
 def DFS(nodeDict, start, end):
     # creates lists for nodes to visit and visited nodes
-    open = []
+    Open = []
     closed = []
 
     # begins with the start node
-    open.append(start)
+    Open.append(start)
 
     # loops through the unvisited nodes until there are no more
-    while open:
+    while Open:
         # examines the node at the top of the stack
-        curNode = open.pop()
+        curNode = Open.pop()
         # checks if the node is found
         if curNode == end:
             # adds the final node and returns the path
@@ -142,38 +120,16 @@ def DFS(nodeDict, start, end):
         elif curNode not in closed:
             # adds the current node to visited nodes
             closed.append(curNode)
-            # adds all neighbors of the current node to unvisited
-            for pair in sorted(nodeDict[curNode]):
-                open.append(pair[0])
+            # checks if the current node has neighbors for directed graphs
+            if curNode in nodeDict.keys():
+                # adds all neighbors of the current node to unvisited
+                for pair in sorted(nodeDict[curNode], reverse=True):
+                    Open.append(pair[0])
 
     # return blank string if none found
-    return " "
+    return ""
+    
 
-
-'''
-Based on pseudocode from Wikipedia
-https://en.wikipedia.org/wiki/Breadth-first_search
-1 Breadth-First-Search(Graph, root):
- 2 
- 3     for each node n in Graph:            
- 4         n.distance = INFINITY        
- 5         n.parent = NIL
- 6 
- 7     create empty queue Q      
- 8 
- 9     root.distance = 0
-10     Q.enqueue(root)                      
-11 
-12     while Q is not empty:        
-13     
-14         current = Q.dequeue()
-15     
-16         for each node n that is adjacent to current:
-17             if n.distance == INFINITY:
-18                 n.distance = current.distance + 1
-19                 n.parent = current
-20                 Q.enqueue(n)
-'''
 # BFS() uses a graph to search breadth first to find a path to a given end node 
 #       from a start node and returns the path as a list
 # Input: nodeDict - a dictionary of nodes representing a graph
@@ -182,19 +138,21 @@ https://en.wikipedia.org/wiki/Breadth-first_search
 # Output: a list of the path from start to end 
 def BFS(nodeDict, start, end):
     # creates the unvisited nodes as a queue
-    open = Queue()
+    Open = Queue()
     # closed 1 is the path taken and closed 2 is the node that led to the next node
+    seen = []
     closed1 = []
     closed2 = []
 
     # begins searching at the start node which is the node and what led to it
-    open.put((start,start))
+    Open.put((start,start))
+    seen.append(start)
 
     # loops until there are no more unvisited nodes
-    while open:
+    while Open:
         # dequeues the first node
-        curNode = open.get()
-
+        curNode = Open.get()
+    
         # checks if the node is at the end and stops if it is
         if curNode[0] == end:
             # adds the final node and what sent it to the lists
@@ -217,46 +175,25 @@ def BFS(nodeDict, start, end):
             # returns the final path reversed for consistency with DFS
             return reversed(final)
 
-        # Adds each of the neighbors of the node if it is not the goal
-        for pair in sorted(nodeDict[curNode[0]]):
-            # each node is classified by the node it is at and the node that led to it
-            open.put((pair[0], curNode[0]))
+        # checks if the current node has neighbors for directed graphs
+        elif curNode[0] in nodeDict.keys():
+            # Adds each of the neighbors of the node if it is not the goal
+            for pair in sorted(nodeDict[curNode[0]]):
+                # must check if it is not in seen in case a previous node added it before closing
+                if pair[0] not in seen:
+                    seen.append(pair[0])
+                    # each node is classified by the node it is at and the node that led to it
+                    Open.put((pair[0], curNode[0]))
+
         
         # updates the visited lists and how they got there
         closed1.append(curNode[0])
         closed2.append(curNode[1])
         
     # return blank string if none found
-    return " "
+    return ""
 
 
-
-'''
-Based on pseudocode from Wikipedia
-https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
- 1  function Dijkstra(Graph, source):
- 2
- 3      create vertex set Q
- 4
- 5      for each vertex v in Graph:             // Initialization
- 6          dist[v] = INFINITY                  // Unknown distance from source to v
- 7          prev[v] = UNDEFINED                 // Previous node in optimal path from source
- 8          add v to Q                          // All nodes initially in Q (unvisited nodes)
- 9
-10      dist[source] = 0                        // Distance from source to source
-11      
-12      while Q is not empty:
-13          u = vertex in Q with min dist[u]    // Source node will be selected first
-14          remove u from Q 
-15          
-16          for each neighbor v of u:           // where v is still in Q.
-17              alt = dist[u] + length(u, v)
-18              if alt < dist[v]:               // A shorter path to v has been found
-19                  dist[v] = alt 
-20                  prev[v] = u 
-21
-22      return dist[], prev[]
-'''
 # UCS() uses a graph to search using Dijkstra's algorithm to find 
 #       a path to a given end node from a start node and returns 
 #       the path as a list
@@ -266,7 +203,7 @@ https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
 # Output: a list of the path from start to end 
 def UCS(nodeDict, start, end):
     # crates the priority queue with a max value of 10,000
-    open = PriorityQueue(10000)
+    Open = PriorityQueue(10000)
     # creates dictionaries to keep track of distance and previous node of each element
     distance = {}
     previous = {}
@@ -282,12 +219,12 @@ def UCS(nodeDict, start, end):
         previous[node] = None
 
         # adds each node to the queue
-        open.put((distance[node], node))
+        Open.put((distance[node], node))
 
     # iterates through each node of the graph
-    while open:
+    while Open:
         # gets the least valued piece from the queue
-        cur = open.get()
+        cur = Open.get()
         
         # checks if reached the end
         if cur[1] == end:
@@ -300,6 +237,8 @@ def UCS(nodeDict, start, end):
             # returns start reverse for consistency
             return reversed(finalPath)
 
+        # list of nodes that are in open that need to be updated
+        openNodes = []
         # Adds each of the neighbors of the node and compares their length
         for pair in sorted(nodeDict[cur[1]]):
             # distance of current path is saved and compared with distance
@@ -310,10 +249,47 @@ def UCS(nodeDict, start, end):
                 distance[pair[0]] = alternate
                 previous[pair[0]] = cur[1]
 
+                # finds if the nodes are in the open queue and adds the new value to temp list
+                if pair[0] in [x[1] for x in Open.queue]:
+                    openNodes.append( (alternate, pair[0]) )
+
+        # list of all the nodes in open including updated ones
+        newOpen = []    
+
+        # dequeues each of the nodes in Open to update the ones that need it
+        for i in range(len(Open.queue)):
+            node = Open.get()
+            if node[1] in [x[1] for x in openNodes]:
+                newOpen.append([x for x in openNodes if x[1] == node[1]][0])
+            else:
+                newOpen.append(node)
+
+        # repopulates Open with updated values
+        for node in newOpen:
+            Open.put(node)
+
+    # end while loop
+
     # returns blank string if no output found
     return ""
 
 
+# writePath() writes the final path it takes to search from start to end to a new file 
+# Input: outFile - the filename of the file to be written to
+#        finalPath - a list of the nodes of the path from start to end nodes
+# Output: None
+def writePath(outFile, finalPath):
+    outFile = open(outFile, 'w')
+
+    if NO_PATH != finalPath:
+        for node in finalPath:
+            outFile.write("{0}\n".format(node))
+    else:
+        outFile.write("No path found")
+        
+    outFile.close()
+    
+    
 # main
 def main(argv):
     # validates amount of arguments given
@@ -327,13 +303,16 @@ def main(argv):
         nodeDict = getNodes(argv[INPUT_FILE], argv[START_NODE], argv[END_NODE])
 
         # validates start and end nodes exist in graph
-        if nodeDict <= MISSING_NODE:
+        if type(nodeDict) == int and nodeDict <= MISSING_NODE:
             if nodeDict == MISSING_START:
                 print("Start node ({0}) is not in the given graph.".format(argv[START_NODE]))        
             elif nodeDict == MISSING_END:
-                print("End node ({0}) is not in the given graph.".format(argv[END_NODE]))        
+                print("End node ({0}) is not in the given graph.".format(argv[END_NODE]))    
+            elif nodeDict == NO_NEIGHBORS:
+                print("Start node ({0}) has no neighbors.".format(argv[START_NODE]))   
             else:
                 print("Start node ({0}) and/or End node ({1}) are not in the given graph.".format(argv[START_NODE], argv[END_NODE]))        
+        
         # checks if file was sucessfully opened
         elif nodeDict != FILE_IO:    
             # performs the search on the graph that the user requests
@@ -347,6 +326,14 @@ def main(argv):
             # writes the final result to the provided file
             writePath(argv[OUTPUT_FILE], finalPath)
 
-
 # call to main
 main(sys.argv)
+
+# old test cases
+#main(['Search.py','test.txt','sup1.txt','A','F', 'DFS'])
+#main(['Search.py','utube.txt','sup2.txt','A','H', 'BFS'])
+#main(['Search.py','other.txt','sup3.txt','S','G', 'DFS'])
+#main(['Search.py','sample.txt','final.txt','1','50', 'BFS'])
+#main(['Search.py','another.txt','one.txt','a','g', 'UCS'])
+
+
